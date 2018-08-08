f039683551ffbb8b45060fe4da328c925117d8b7
Leonardo Augusto Perrella <leonardo.perrella@yahoo.com.br>
2018-08-07 21:03:15 -0300

    eventex

--- eventex/settings.py
+++ eventex/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 AWS_STORAGE_BUCKET_NAME = 'staticfileswttd'
